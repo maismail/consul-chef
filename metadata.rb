cdb96c75be5acf761f253266012f1be2a6573c17
Antonios Kouzoupis <kouzoupis.ant@gmail.com>
2020-02-04 16:10:33 +0100

    [master] Add dependencies

--- metadata.rb
+++ metadata.rb
@@ -11,3 +11,31 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 
 depends 'conda'
 depends 'kagent'
+
+attribute "consul/user",
+          :description => "System user to run the service",
+          :type => 'string'
+
+attribute "consul/group",
+          :description => "Group id of consul user",
+          :type => 'string'
+
+attribute "consul/version",
+          :description => "Version of Consul to install",
+          :type => 'string'
+
+attribute "consul/use_dnsmasq",
+          :description => "Flag to control installation of dnsmasq. If set to false, DNS service discovery will NOT work",
+          :type => 'string'
+
+attribute "consul/bind_address",
+          :description => "IP address Consul agent will bind to. You can also set a go-sockaddr template. Check https://www.consul.io/docs/agent/options.html#_bind for more information.",
+          :type => 'string'
+
+attribute "consul/master/bootstrap_expect",
+          :description => "Number of Consul masters to wait before it starts serving requests",
+          :type => 'string'
+
+attribute "consul/master/ui",
+          :description => "Flag to enable/disable the web UI",
+          :type => 'string'
