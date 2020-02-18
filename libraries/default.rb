ed879bb21c8087d74421cbaf04f0e0e9e88aad87
Antonios Kouzoupis <kouzoupis.ant@gmail.com>
2020-02-18 14:09:06 +0100

    [master] Add library to get service ip and port based on service name and tags

---
+++ libraries/default.rb
@@ -0,0 +1,61 @@
+require 'openssl'
+require 'net/http'
+require 'uri'
+require 'json'
+
+class ConsulHelper
+    def initialize(node)
+        @node = node
+    end
+
+    def get_service(name, tags)
+        raw_certificate = File.read @node['kagent']['certs']['elastic_host_certificate']
+        certificate = OpenSSL::X509::Certificate.new raw_certificate
+
+        raw_key = File.read @node['kagent']['certs']['host_key']
+        key = OpenSSL::PKey::RSA.new raw_key
+
+        Net::HTTP.start(
+            "localhost",
+            @node['consul']['http_api_port'],
+            :use_ssl => true,
+            :verify_mode => OpenSSL::SSL::VERIFY_NONE,
+            :cert => certificate,
+            :key => key,
+        ) do |http|
+            uri = URI("/v1/health/service/#{name}")
+            filter_tags = []
+            tags.each { |st| filter_tags.push("\"#{st}\" in Service.Tags")}
+            service_tags_filter = filter_tags.join(" and ")
+            http_params = {
+                :passing => true,
+                :filter => service_tags_filter
+            }
+            uri.query = URI.encode_www_form(http_params)
+            response = http.request_get(uri.to_s)
+            if !response.kind_of? Net::HTTPSuccess
+                raise "Error contacting local Consul agent Status: #{response.code} Reason: #{response.msg}"
+            end
+            body = JSON.parse(response.body)
+            if body.empty?
+                return nil, nil
+            end
+            return body[0]['Node']['Address'], body[0]['Service']['Port']
+        end
+    end
+
+end
+
+class Chef
+    class Recipe
+        def consul_helper
+            ConsulHelper.new(node)
+        end
+    end
+
+    class Resource
+        def consul_helper
+            ConsulHelper.new(node)
+        end
+    end
+end
